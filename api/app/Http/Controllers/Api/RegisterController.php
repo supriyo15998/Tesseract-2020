b30dc42ca52b743e09ccd99af3f99901c8b7447c
supriyo15998 <supriyo15998@gmail.com>
2020-02-07 22:21:31 +0530

    Initial commit

---
+++ api/app/Http/Controllers/Api/RegisterController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Volunteer;
+class RegisterController extends Controller
+{
+    public function registerV(Request $request)
+    {
+    	$validatedData = $request->validate([
+    		'name' => 'required',
+    		'college_roll' => 'required|max:12|min:12',
+    		'department' => 'required',
+    		'college_id' => 'required|regex:[Gg][Nn][Ii][Tt]+\/+[0-9]{4}\/+[0-9]{4}',
+    		'email' => 'required',
+    		'phone' => 'required|max:10|min:10',
+    		'field_of_interest' => 'required'
+    	]);
+    	$volunteer = Volunteer::create($validatedData);
+        return response()->json([
+            'volunteer' => $volunteer
+        ]);
+    }
+}
---
+++ api/database/migrations/2020_02_07_164417_create_volunteers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVolunteersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('volunteers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('college_roll');
+            $table->string('department');
+            $table->string('college_id');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('field_of_interest');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('volunteers');
+    }
+}
