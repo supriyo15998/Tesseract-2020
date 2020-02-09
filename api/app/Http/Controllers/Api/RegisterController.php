f7ad9af3482cdd9c4686dbd78f0302ada537a0df
Faraz Ali <farazappy@esportsindia.in>
2020-02-09 17:51:22 +0530

    Changes

--- api/app/Http/Controllers/Api/RegisterController.php
+++ api/app/Http/Controllers/Api/RegisterController.php
@@ -12,7 +12,6 @@ class RegisterController extends Controller
     	$validatedData = $request->validate([
     		'name' => 'required',
     		'college_roll' => 'required|max:12|min:12|unique:volunteers',
-    		'department' => 'required',
             'year' => 'required',
     		'college_id' => 'required|regex:/[Gg][Nn][Ii][Tt]+\/+[0-9]{4}\/+[0-9]{4}$/|unique:volunteers',
     		'email' => 'required|unique:volunteers|email',
