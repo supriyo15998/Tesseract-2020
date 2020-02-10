3632d0caef3669a7404a8845c8a88c607f1abfbf
= <=>
2020-02-10 15:39:23 +0530

    Changes

--- api/app/Http/Controllers/Api/RegisterController.php
+++ api/app/Http/Controllers/Api/RegisterController.php
@@ -14,7 +14,7 @@ class RegisterController extends Controller
     		'college_roll' => 'required|max:12|min:12|unique:volunteers',
 			'year' => 'required',
 			'department_id' => 'required',
-    		'college_id' => 'required|regex:/[Gg][Nn][Ii][Tt]+\/+[0-9]{4}\/+[0-9]{4}$/|unique:volunteers',
+    		'college_id' => 'required|regex:/^[Gg][Nn][Ii][Tt]+\/+[0-9]{4}\/+[0-9]{4}$/|unique:volunteers',
     		'email' => 'required|unique:volunteers|email',
     		'phone' => 'required|digits:10',
     		'field_of_interest' => 'required'
