6007aeceff08e79da94330184f06ace8ea6475b0
Faraz Ali <farazappy@esportsindia.in>
2020-02-13 23:18:11 +0530

    Changes

--- api/app/Category.php
+++ api/app/Category.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $guarded = [];
+
+    public function events() {
+        return $this->hasMany('App\Event');
+    }
 }
