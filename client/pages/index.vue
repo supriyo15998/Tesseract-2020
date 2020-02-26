057107ff3405f042b636a01d91ca10882520c506
= <=>
2020-02-26 16:05:49 +0530

    Changes

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -145,8 +145,27 @@
                         <div class="col-lg-12 col-md-6 text-center">
                             <h2>Highlighted Events</h2>
                         </div>
-                        <div class="col-lg-2"></div>
                         <div
+                            class="col-lg-4"
+                        >   <center>
+                            <div class="speaker">
+                                <img
+                                    src="https://downloads.tesseractgnit.com/img/events/1582639374_8f736f632d3a003e.jpg"
+                                    alt="Speaker 1"
+                                    class="img-fluid"
+                                >
+                                <div class="details">
+                                    <h3>
+                                        <nuxt-link
+                                            to="/gaming/pubg-mobile"
+                                            style="text-decoration: none"
+                                        >PUBG (Mobile) Squad</nuxt-link>
+                                    </h3>
+                                    <p>PLAYERUNKNOWN'S BATTLEGROUNDS (PUBG) is a competitive survival shooting game.</p>
+                                </div>
+                            </div>
+                            </center>
+                        </div>                        <div
                             class="col-lg-4"
                         >   <center>
                             <div class="speaker">
@@ -179,9 +198,9 @@
                                 <div class="details">
                                     <h3>
                                         <nuxt-link
-                                            to="/gaming/pubg-mobile"
+                                            to="/gaming/pubg-mobile-solo"
                                             style="text-decoration: none"
-                                        >PUBG Mobile</nuxt-link>
+                                        >PUBG (Mobile) Solo</nuxt-link>
                                     </h3>
                                     <p>PLAYERUNKNOWN'S BATTLEGROUNDS (PUBG) is a competitive survival shooting game.</p>
                                 </div>
