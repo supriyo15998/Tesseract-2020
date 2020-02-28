9f130d736573e91fb1de6998ba7efcea55306cd4
Faraz Ali <farazappy@esportsindia.in>
2020-02-28 23:08:50 +0530

    Changes

--- api/resources/views/home.blade.php
+++ api/resources/views/home.blade.php
@@ -64,9 +64,9 @@
         <div class="col-lg-3 col-xs-6">
           <div class="small-box bg-green">
             <div class="inner">
-              <h3>0</h3>
+              <h3>{{ $naveenCount }}</h3>
 
-              <p>Participants Registered</p>
+              <p>First Year Nibbas Registered</p>
             </div>
             <div class="icon">
               <i class="ion ion-stats-bars"></i>
