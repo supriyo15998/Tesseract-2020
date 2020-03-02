d482bb2334160ac2a90d516bd2396ccd48f87e1f
Faraz Ali <farazappy@esportsindia.in>
2020-03-03 01:03:26 +0530

    ChangeS

--- api/app/Http/Controllers/Api/HomeController.php
+++ api/app/Http/Controllers/Api/HomeController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function getOrderDetails($orderId) {
-        $order = \App\Order::with('events')->with(array('team' => function($q) { $q->with('leader')->with('members'); }))->firstOrFail($orderId);
+        $order = \App\Order::with('events')->with(array('team' => function($q) { $q->with('leader')->with('members'); }))->findOrFail($orderId);
         return response()->json([
             'success' => [
                 'order' => $order
