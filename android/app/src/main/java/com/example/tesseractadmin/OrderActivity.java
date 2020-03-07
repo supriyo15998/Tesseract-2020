7d21ba6042929e7f43c8da34216f278bfac9caeb
Faraz Ali <farazappy@esportsindia.in>
2020-03-07 23:11:46 +0530

    Changes

--- android/app/src/main/java/com/example/tesseractadmin/OrderActivity.java
+++ android/app/src/main/java/com/example/tesseractadmin/OrderActivity.java
@@ -21,6 +21,7 @@ public class OrderActivity extends AppCompatActivity {
 
     private Order order;
     private TableLayout membersTable, eventsTable;
+    int i = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,63 +38,11 @@ public class OrderActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        int i = 1;
 
-        for (Member member : order.getTeam().getMembers()) {
-                TableRow tr = new TableRow(this);
-                tr.setId(i);
-
-                TableRow.LayoutParams param = new TableRow.LayoutParams(
-                        TableRow.LayoutParams.WRAP_CONTENT,
-                        TableRow.LayoutParams.WRAP_CONTENT,
-                        1
-                );
-                param.setMargins(5, 5, 5, 5);
-
-                TextView t1 = new TextView(this);
-                t1.setId(i+222);
-                t1.setLayoutParams(param);
-                t1.setGravity(Gravity.CENTER);
-                t1.setText(member.getName());
-                t1.setTextSize(17);
-
-                TextView t2 = new TextView(this);
-                t2.setId(i+333);
-                t2.setLayoutParams(param);
-                t2.setGravity(Gravity.CENTER);
-                t2.setText(member.getCollegeId());
-                t2.setTextSize(17);
-
-                TextView t3 = new TextView(this);
-                t3.setId(i+444);
-                t3.setLayoutParams(param);
-                t3.setGravity(Gravity.CENTER);
-                t3.setText(member.getCollege());
-                t3.setTextSize(17);
-
-                TextView t = new TextView(this);
-                t.setId(i+111);
-                t.setLayoutParams(param);
-                t.setGravity(Gravity.CENTER);
-                t.setText(member.getYear());
-                t.setTextSize(17);
-
-                if (member.getId() .equals(order.getTeam().getLeader().getId())) {
-                    tr.setBackgroundColor(getResources().getColor(R.color.colorAccent));
-                    t.setTextColor(getResources().getColor(android.R.color.white));
-                    t1.setTextColor(getResources().getColor(android.R.color.white));
-                    t2.setTextColor(getResources().getColor(android.R.color.white));
-                    t3.setTextColor(getResources().getColor(android.R.color.white));
-                }
-
-                tr.addView(t1);
-                tr.addView(t2);
-                tr.addView(t3);
-                tr.addView(t);
-
-                membersTable.addView(tr);
-                i++;
-        }
+        if(order.isTeam())
+            populateTeam();
+        else
+            populateSolo();
 
         for (Event event : order.getEvents()) {
             TableRow tr = new TableRow(this);
@@ -148,9 +97,71 @@ public class OrderActivity extends AppCompatActivity {
             eventsTable.addView(tr);
             i++;
         }
+    }
+
+    private void populateTeam() {
+        for (Member member : order.getTeam().getMembers()) {
+            TableRow tr = new TableRow(this);
+            tr.setId(i);
+
+            TableRow.LayoutParams param = new TableRow.LayoutParams(
+                    TableRow.LayoutParams.WRAP_CONTENT,
+                    TableRow.LayoutParams.WRAP_CONTENT,
+                    1
+            );
+            param.setMargins(5, 5, 5, 5);
+
+            TextView t1 = new TextView(this);
+            t1.setId(i+222);
+            t1.setLayoutParams(param);
+            t1.setGravity(Gravity.CENTER);
+            t1.setText(member.getName());
+            t1.setTextSize(17);
+
+            TextView t2 = new TextView(this);
+            t2.setId(i+333);
+            t2.setLayoutParams(param);
+            t2.setGravity(Gravity.CENTER);
+            t2.setText(member.getCollegeId());
+            t2.setTextSize(17);
+
+            TextView t3 = new TextView(this);
+            t3.setId(i+444);
+            t3.setLayoutParams(param);
+            t3.setGravity(Gravity.CENTER);
+            t3.setText(member.getCollege());
+            t3.setTextSize(17);
+
+            TextView t = new TextView(this);
+            t.setId(i+111);
+            t.setLayoutParams(param);
+            t.setGravity(Gravity.CENTER);
+            t.setText(member.getYear());
+            t.setTextSize(17);
+
+            if (member.getId() .equals(order.getTeam().getLeader().getId())) {
+                tr.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                t.setTextColor(getResources().getColor(android.R.color.white));
+                t1.setTextColor(getResources().getColor(android.R.color.white));
+                t2.setTextColor(getResources().getColor(android.R.color.white));
+                t3.setTextColor(getResources().getColor(android.R.color.white));
+            }
+
+            tr.addView(t1);
+            tr.addView(t2);
+            tr.addView(t3);
+            tr.addView(t);
+
+            membersTable.addView(tr);
+            i++;
+        }
 
         TextView teamName = findViewById(R.id.teamName);
         teamName.setText(order.getTeam().getName());
+    }
+
+    private void populateSolo() {
+        Member member = order.get
     }
 
     @Override
