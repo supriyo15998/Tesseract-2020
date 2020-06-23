733654188b429eb463284f4dee61c7790ef036a6
supriyo15998 <supriyo15998@gmail.com>
2020-06-23 19:51:08 +0530

    changes

--- client/pages/validate/certificate/_id.vue
+++ client/pages/validate/certificate/_id.vue
@@ -1,5 +1,13 @@
 <template>
-    <h1>This certificate was awarded to {{ user.name }}</h1>
+    <b-card-text>
+        <b-card class="text-center" overlay img-src="../../../static/img/bill_back.jpg" style="padding: 15%">
+            <b-img src="../../../static/img/logo_dark.jpg" fluid alt="Responsive image"></b-img>
+            <!-- <div class="bg-success text-light"> -->
+                <!-- <b-img src="../../../static/tesseract_transparent_small.png" height="100" width="100" alt="Responsive image"></b-img> -->
+                <h1>This certificate was awarded to {{ user.name }}</h1>
+            <!-- </div> -->
+        </b-card>
+    </b-card-text>
 </template>
 
 <script>
