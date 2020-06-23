3581d18e786ee16d75f52e05411a1c5eeffe28a1
Faraz Ali <farazappy@esportsindia.in>
2020-06-23 23:29:16 +0530

    Changes

--- client/pages/validate/certificate/_id.vue
+++ client/pages/validate/certificate/_id.vue
@@ -1,13 +1,24 @@
 <template>
-    <b-card-text>
-        <b-card class="text-center" overlay img-src="../../../static/img/bill_back.jpg" style="padding: 15%">
-            <b-img src="../../../static/img/logo_dark.jpg" fluid alt="Responsive image"></b-img>
-            <!-- <div class="bg-success text-light"> -->
-                <!-- <b-img src="../../../static/tesseract_transparent_small.png" height="100" width="100" alt="Responsive image"></b-img> -->
-                <h1>This certificate was awarded to {{ user.name }}</h1>
-            <!-- </div> -->
-        </b-card>
-    </b-card-text>
+    <div
+        class="text-center"
+        style="border: 1px dashed; margin: 5%;padding: 10%; padding-bottom: 5%; padding-top: 5%;"
+    >
+        <img src="@/static/img/logo_dark.jpg" />
+        <p class="lead">This certificate was awarded to</p>
+        <h1 class="display-3">{{ user.name }}</h1>
+        <p class="lead">Verified by Team Tesseract - <b>Guru Nanak Institute of Technology</b></p>
+        <p class="lead">For more information please contact <a href="mailto:admin@tesseractgnit.com"><b>admin@tesseractgnit.com</b></a></p>
+        <!-- <b-jumbotron>
+
+            <template v-slot:header><span style="position: absolute; margin-top: 2%;">{{ user.name }}</span></template>
+
+            <template v-slot:lead>
+                This certificate was awarded to
+            </template>
+
+            <p style="position: absolute; right: 5%;">Validated by the Tesseract GNIT Team</p>
+        </b-jumbotron> -->
+    </div>
 </template>
 
 <script>
