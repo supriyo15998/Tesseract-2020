3b1ce2f2e60643575b0413bb3fb176ce11d8e914
Faraz Ali <farazappy@esportsindia.in>
2020-06-24 22:38:55 +0530

    Changes

--- api/routes/web.php
+++ api/routes/web.php
@@ -40,6 +40,121 @@ Route::domain('downloads.tesseractgnit.com')->group(function() {
 // Route::get('/test', 'PDFController@generatePdfForTeam')->name('generatePdfForTeam');
 // Route::get('/solo', 'PDFController@generatePdfSolo')->name('generatePdfSolo');
 
+Route::get('test-test', function() {
+	$recipients = [
+			'soumyajit9051@gmail.com',
+			'abhishekmaheshwari.01062000@gmail.com',
+			'souhardyamanna77@gmail.com',
+			'sayanikarmakar444@gmail.com',
+			'moumita29kar@gmail.com',
+			'shreejitbhattacharyya@gmail.com',
+			'1trimbak@gmail.com',
+			'ritesh.sanyal.mankar@gmail.com',
+			'satyakisinhastar@gmail.com',
+			'kathaghosh23@gmail.com',
+			'aanchalguptakol@gmail.com',
+			'bhattacharyyarup32@gmail.com',
+			'asthaghosh911@gmail.com',
+			'mariananikpal@gmail.com',
+			'gsayantan01@gmail.com',
+			'das.soumava@gmail.com',
+			'arthamrocks@gmail.com',
+			'dassohom5@gmail.com',
+			'kushalbanik93@gmail.com',
+			'robosapienz18@gmail.com',
+			'robosapienz18@gmail.com',
+			'robosapienz18@gmail.com',
+			'robosapienz18@gmail.com',
+			'robosapienz18@gmail.com',
+			'avikboseofficial1999@gmail.com',
+			'avikboseofficial1999@gmail.com',
+			'avikboseofficial1999@gmail.com',
+			'avikboseofficial1999@gmail.com',
+			'aritrarakshit1@gmail.com',
+			'aritrarakshit1@gmail.com',
+			'aritrarakshit1@gmail.com',
+			'aritrarakshit1@gmail.com',
+			'smitabhushan58@gmail.com',
+			'pratikshad2000@gmail.com',
+			'jeetchakraborty2000@gmail.com',
+			'raunak.chakraborty123@gmail.com',
+			'duttasubhajit956@gmail.com',
+			'sdhang1999@gmail.com',
+			'etimukhetjee73@gmail.com',
+			'patrashuvam53@gmail.com',
+			'samarjitkundu99@gmail.com',
+			'madhabikundu008@gmail.com',
+			'dibyendusaha388@gmail.com',
+			'sattyajitt1997@gmail.com',
+			'kaustabhadak30@gmail.com',
+			'pujapalodhi@gmail.com',
+			'akashsamanta100@gmail.com',
+			'samibanerjee1@gmail.com',
+			'shubhrajitpallob@gmail.com',
+			'sagar.argon92@gmail.com',
+			'rounakghosh42@gmail.com',
+			'romikbanerjee2512@gmail.com',
+			'sayak2000saha@gmail.com',
+			'sayanmajumder666@gmail.com',
+			'uddipanc0@gmail.com',
+			'bhowmicksayan0001@gmail.com',
+			'91mondal.sayan@gmail.com',
+			'1999rittikachakraborty@gmail.com',
+			'nm7278490606@gmail.com',
+			'mukherjeerumon26@gmail.com',
+			'1904088@kiit.ac.in',
+			'1904219@kiit.ac.in',
+			'19042387@kiit.ac.in',
+			'1904387@kiit.ac.in',
+			'parthib2001@gmail.com',
+			'sohamvictorfernandez@gmail.com',
+			'romitroy1999@gmail.com',
+			'bhattacharjeesayan1099@gmail.com',
+			'anish1999das@gmail.com',
+			'alangkrita9876@gmail.com',
+			'lipikamukherjee99@gmail.com',
+			'tua3642@gmail.com',
+			'jayashreesarkar1999@gmail.com',
+			'amartyaduttabanik@gmail.com',
+			'sangram.2015patra@gmail.com',
+			'mail420pritamsamanta@gmail.com',
+			'adrijaspriha.16@gmail.com',
+			'mrohit724@gmail.com',
+			'amartyaduttabanik@gmail.com',
+			'sangram.2015patra@gmail.com',
+			'mail420pritamsamanta@gmail.com',
+			'adrijaspriha.16@gmail.com',
+			'mrohit724@gmail.com',
+			'arnabdas199920@gmail.com',
+			'pralayghoshg9@gmail.com',
+			'debjyotighosh9711@gmail.com',
+			'arnab0553@gmail.com',
+			'akashroydtk22@gmail.com',
+			'sagar.argon92@gmail.com',
+			'romikbanerjee2512@gmail.com',
+			'rounakghosh42@gmail.com',
+			'sayak2000saha@gmail.com',
+			'shubhrajitpallob@gmail.com',
+			'sagnikbhowmik00@gmail.com',
+			'shreya7.roy@gmail.com',
+			'roysouradeep2000@gmail.com',
+			'soumyabiswassb@gmail.com',
+			'1082002vibhavsingh@gmail.com',
+			'1904387@kiit.ac.in',
+			'1904088@kiit.ac.in',
+			'1904219@kiit.ac.in',
+			'rishadey2016@gmail.com',
+			'digantadutta96@gmail.com',
+			'deepbanerjee68@gmail.com'
+		];
+
+		$orders = \App\Order::whereHas('events', function($q) { $q->where('played', 1)->whereIn('event_id', [3, 4, 5, 6, 7]); })->whereHas('team', function($q) { $q->where('is_naveen', 0)->whereHas('participant', function($q) { $q->whereIn('email', $recipients); } );})->get();
+
+		dd(count($orders));
+
+		return "Kya bantai";
+});
+
 Route::get('test', function() {
 	// $orders = [390];
 
@@ -71,19 +186,19 @@ Route::get('test', function() {
 	// }
 	
 
-	foreach($orders as $o) {
+	// foreach($orders as $o) {
 
-		if($o->is_team) {
-			foreach($o->team->members as $participant) {
-				if($o->team->is_naveen)
-					Mail::to($participant->email)->send(new \App\Mail\NaveenCertificate($participant));
-				else
-					Mail::to($participant->email)->send(new \App\Mail\ParticipationCertificate($o->events, $participant));
-			}
-		} else {
-			Mail::to($o->participant->email)->send(new \App\Mail\ParticipationCertificate($o->events, $o->participant));
-		}
-	}
+	// 	if($o->is_team) {
+	// 		foreach($o->team->members as $participant) {
+	// 			if($o->team->is_naveen)
+	// 				Mail::to($participant->email)->send(new \App\Mail\NaveenCertificate($participant));
+	// 			else
+	// 				Mail::to($participant->email)->send(new \App\Mail\ParticipationCertificate($o->events, $participant));
+	// 		}
+	// 	} else {
+	// 		Mail::to($o->participant->email)->send(new \App\Mail\ParticipationCertificate($o->events, $o->participant));
+	// 	}
+	// }
 
 	return 'Done';
 });
