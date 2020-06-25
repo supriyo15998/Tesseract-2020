c461eca59d2a271b7a1c716252f83a06b5f3e9fc
Faraz Ali <farazappy@esportsindia.in>
2020-06-25 13:05:11 +0530

    Changes

--- api/routes/web.php
+++ api/routes/web.php
@@ -177,7 +177,9 @@ Route::get('test', function() {
 	// foreach($emails as $email)
 	// 	Mail::to($email)->send(new \App\Mail\TestMail());
 
-	$orders = \App\Order::whereHas('events', function($q) { $q->where('played', 1)->whereIn('event_id', [8, 9, 10]); })->whereHas('team', function($q) { $q->where('is_naveen', 0)->whereHas('members', function($q) { $q->where('certificate_sent', 0); });})->get();
+	//$orders = \App\Order::whereHas('events', function($q) { $q->where('played', 1)->whereIn('event_id', [8, 9, 10]); })->whereHas('team', function($q) { $q->where('is_naveen', 0)->whereHas('members', function($q) { $q->where('certificate_sent', 0); });})->get();
+	
+	$orders = \App\Order::whereHas('events', function($q) { $q->where('paid', 1)->whereIn('event_id', [19, 20, 21, 22]); })->where(function ($q) { $q->whereHas('team', function($q) { $q->where('is_naveen', 1)->whereHas('members', function($q) { $q->where('certificate_sent', 0); });})->orWhereHas('participant', function($q) { $q->where('certificate_sent', 0); }); })->get();
 
 	// $o = $orders[0];
 	
