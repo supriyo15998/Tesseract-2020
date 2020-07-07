72a31ec5e9678923ea61ceb8f1aa1b381c4fb443
Faraz Ali <farazappy@esportsindia.in>
2020-07-07 15:27:49 +0530

    Changes

--- api/app/Participant.php
+++ api/app/Participant.php
@@ -7,8 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Participant extends Model
 {
     protected $guarded = [];
+    
+    public $additional_attributes = ['email_name'];
 
     public function team() {
         return $this->belongsTo('App\Team');
+    }
+
+    public function getEmailNameAttribute() {
+        return "Name: {$this->name}, Email: {$this->email}";
     }
 }
